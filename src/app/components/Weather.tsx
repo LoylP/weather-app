7a8d980c2e865d07fa0ebb01db4b53f3e2151d28
LoylP <nhphuc183@gmail.com>
2024-09-07 17:21:17 +0700

    Add components for weather app

---
+++ src/app/components/Weather.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Weather = ({ data }) => {
+    console.log(data);
+    return (
+        <>
+        </>
+    );
+  };
+  
+  export default Weather;
