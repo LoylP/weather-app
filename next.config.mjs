04b6f46bf342664fdde97d9e0f7e918049a14ab7
LoylP <nhphuc183@gmail.com>
2024-09-08 21:21:02 +0700

    Deploy with docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['p1.pxfuel.com', 'api.openweathermap.org', 'openweathermap.org'],
-    },
-  }
-  
+  images: {
+    domains: ["p1.pxfuel.com", "api.openweathermap.org", "openweathermap.org"],
+  },
+  output: "standalone",
+};
+
 export default nextConfig;
